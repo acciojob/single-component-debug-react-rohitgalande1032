023ac749cee53d12ae029d0485b4f8d36644c57b
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-08 16:10:52 +0530

    changes made in index.js and App.jsx

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
+//setup with application with the help of id root
+const root = document.getElementById('root')
+ReactDOM.render(<App />,root)
 
